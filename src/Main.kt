1de8f8d880ba632041e6a7db16e0e1dc564b64da
asarycheva98-lgtm <a.sarycheva98@gmail.com>
2025-12-03 18:46:37 +0600

    ...

--- src/Main.kt
+++ src/Main.kt
@@ -5,6 +5,12 @@ import java.util.Scanner
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+    /*time()
+    wheather()*/
+    calculate()
+}
+
+fun time() {
     val timeInput = Scanner(System.`in`).nextLine()
     try {
         val time = LocalTime.parse(timeInput)
@@ -17,7 +23,6 @@ fun main() {
         println("Фигню написал")
     }
 
-    wheather()
 }
 
 fun wheather() {
@@ -29,4 +34,42 @@ fun wheather() {
         temperature >= 22 -> println("Жара")
     }
 }
+fun calculate() {
+    println("Напиши цифры свои красивые")
+    val input = readLine()!!.replace(" ", "")
+    val operator = input.find { it == '+' || it == '-' || it == '*' || it == '/' }
+    if (operator == null) {
+        println("Фигню написал")
+        return
+    }
+    val parts = input.split(operator)
+    if (parts.size != 2) {
+        println("Ты дурачок? Выражение должно быть a+b")
+        return
+    }
+    val aNullable: Double? = parts[0].toDoubleOrNull()
+    val bNullable: Double? = parts[1].toDoubleOrNull()
+    if (aNullable == null || bNullable == null) {
+        println("Опять фигню пишешь")
+    }
+    val a: Double = aNullable!!
+    val b: Double = bNullable!!
+    val result = when (operator) {
+        '+' -> a + b
+        '-' -> a - b
+        '*' -> a * b
+        '/' -> {
+            if (b == 0.0) {
+                println("Ну ема, кто на ноль делит...")
+                return
+            }
+            a / b
+        }
 
+        else -> {
+            println("Фигню написал")
+            return
+        }
+    }
+    println("Результат $result")
+}
